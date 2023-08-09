640a430274b4e7e123e7039dbcbed30fa69383c0
Brad Montgomery <brad@bradmontgomery.net>
2023-08-08 21:05:28 -0500

    added an Equalizer component

---
+++ src/Equalizer.jsx
@@ -0,0 +1,23 @@
+import EqualizerBand from './EqualizerBand';
+import './App.css';
+
+export default function Equalizer() {
+    return (
+        <div className="band-container">
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+        <EqualizerBand />
+      </div>
+    )
+}
